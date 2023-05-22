7928398dd84da0b1cc18a71e145fce79191da048
tracer-ics2017 <tracer@njuics.org>
2023-05-22 16:11:16 +0800

    >  compile 2013544 user Linux user-VirtualBox 3.19.0-31-generic #36~14.04.1-Ubuntu SMP Thu Oct 8 17:59:01 UTC 2015 i686 i686 i686 GNU/Linux  16:11:16 up  1:05,  2 users,  load average: 1.58, 0.99, 0.65 795b3237a4367fa390eea8c2311684ad4c3af6d6

--- nemu/include/memory/memory.h
+++ nemu/include/memory/memory.h
@@ -14,5 +14,5 @@ uint32_t vaddr_read(vaddr_t, int);
 uint32_t paddr_read(paddr_t, int);
 void vaddr_write(vaddr_t, int, uint32_t);
 void paddr_write(paddr_t, int, uint32_t);
-
+paddr_t page_translate(vaddr_t addr, int rw);
 #endif
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -32,12 +33,74 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     mmio_write(addr, len, data, map_NO);
   }
 }
+paddr_t page_translate(vaddr_t addr, int rw) {
+  if (!cpu.cr0.protect_enable || !cpu.cr0.paging)
+    return addr;
+
+  PDE* pd = (PDE*)(cpu.cr3.val & 0xfffff000);
+  int pd_index = (addr >> 22) & 0x3ff;
+  PTE* pt;
+  
+  PDE pde;
+  pde.val = paddr_read((paddr_t)&pd[pd_index], 4);
+  if (!pde.present)
+    Assert(0, "pde present bit is 0!");
+
+  pt = (PTE*)(pde.val & ~0xfff);
+  pde.accessed = 1;
+  paddr_write((paddr_t)&pd[pd_index], 4, pde.val);
+
+  int pt_index = (addr >> 12) & 0x3ff;
+  PTE pte;
+  pte.val = paddr_read((paddr_t)&pt[pt_index], 4);
+  if (!pte.present)
+    Assert(0, "pte present bit is 0!");
+
+  addr = (pte.val & ~0xfff) | (addr & 0xfff);
+  pte.accessed = 1;
+  if (rw)
+    pte.dirty = 1;
+  paddr_write((paddr_t)&pt[pt_index], 4, pte.val);
+
+  return (paddr_t)addr;
+}
+
 
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    // 数据跨页边界
+    int i;
+    uint32_t data = 0;
+    for (i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, 0);
+      data += paddr_read(paddr, 1) << (i * 8);
+    }
+    return data;
+  } else {
+    paddr_t paddr = page_translate(addr, 0);
+    return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    // 数据跨页边界
+    int i;
+    for (i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, 1);
+      paddr_write(paddr, 1, data >> (i * 8));
+    }
+  } else {
+    paddr_t paddr = page_translate(addr, 1);
+    paddr_write(paddr, len, data);
+  }
 }
+
+// uint32_t vaddr_read(vaddr_t addr, int len) {
+//   return paddr_read(addr, len);
+// }
+
+// void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+//   paddr_write(addr, len, data);
+// }
