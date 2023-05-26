481ebef45df96b583e0f41ae3629966c93af369d
tracer-ics2017 <tracer@njuics.org>
2023-05-26 11:20:46 +0800

    >  run 2013544 user Linux user-VirtualBox 3.19.0-31-generic #36~14.04.1-Ubuntu SMP Thu Oct 8 17:59:01 UTC 2015 i686 i686 i686 GNU/Linux  11:20:46 up 24 min,  1 user,  load average: 1.50, 0.70, 0.42 b2d99ab4549a11fdadf98a2fd2fc405881df258

--- navy-apps/apps/pal/build/x86/src/FLOAT/FLOAT.d
+++ navy-apps/apps/pal/build/x86/src/FLOAT/FLOAT.d
@@ -1,6 +1,5 @@
-/home/user/ics2017/ics2017/navy-apps/apps/pal/build/x86//src/FLOAT/FLOAT.o: \
- src/FLOAT/FLOAT.c \
- /home/user/ics2017/ics2017/navy-apps/apps/pal/include/FLOAT.h \
- /home/user/ics2017/ics2017/navy-apps/libs/libc/include/assert.h \
- /home/user/ics2017/ics2017/navy-apps/libs/libc/include/_ansi.h \
- /home/user/ics2017/ics2017/navy-apps/libs/libc/include/sys/config.h
+/home/user/ics2017/navy-apps/apps/pal/build/x86//src/FLOAT/FLOAT.o: \
+ src/FLOAT/FLOAT.c /home/user/ics2017/navy-apps/apps/pal/include/FLOAT.h \
+ /home/user/ics2017/navy-apps/libs/libc/include/assert.h \
+ /home/user/ics2017/navy-apps/libs/libc/include/_ansi.h \
+ /home/user/ics2017/navy-apps/libs/libc/include/sys/config.h
--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,15 +1,33 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  //assert(0);
+  //return 0;
+  return ((int64_t)a * (int64_t)b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
   assert(0);
   return 0;
+  // assert(b != 0);
+  // FLOAT x = Fabs(a);
+  // FLOAT y = Fabs(b);
+  // FLOAT ret = x / y;
+  // x = x % y;
+
+  // for (int i = 0; i < 16; i++) {
+  //   x <<= 1;
+  //   ret <<= 1;
+  //   if (x >= y) {
+  //     x -= y;
+  //     ret++;
+  //   }
+  // }
+  // if (((a ^ b) & 0x80000000) == 0x80000000) {
+  //   ret = -ret;
+  // }
+  // return ret;
 }
 
 FLOAT f2F(float a) {
@@ -25,12 +43,43 @@ FLOAT f2F(float a) {
 
   assert(0);
   return 0;
+  // union float_ {
+  //   struct {
+  //     uint32_t man : 23;
+  //     uint32_t exp : 8;
+  //     uint32_t sign : 1;
+  //   };
+  //   uint32_t val;
+  // };
+  // union float_ f;
+  // f.val = *((uint32_t*)(void*)&a);
+  // int exp = f.exp - 127;
+  // FLOAT ret = 0;
+  // if (exp == 128)
+  //   assert(0);
+  // if (exp >= 0) {
+  //   int mov = 7 - exp;
+  //   if (mov >= 0)
+  //     ret = (f.man | (1 << 23)) >> mov;
+  //   else
+  //     ret = (f.man | (1 << 23)) << (-mov);
+  // }
+  // else
+  //   return 0;
+  // return f.sign == 0 ? ret : -ret;
 }
 
 FLOAT Fabs(FLOAT a) {
   assert(0);
   return 0;
+  // if ((a & 0x80000000) == 0)
+  //   return a;
+  // else
+  //   return -a;
 }
+
+
+
 
 /* Functions below are already implemented */
 
